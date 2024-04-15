9b2ae9ed8f0464f833b92065b6d0bbdfc54bb5ac
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-04-14 22:57:16 -0600

    Add cart provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { FilterProvider } from './context/filters.jsx'
+import { CartProvider } from './context/cart.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FilterProvider >
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </FilterProvider>
   </React.StrictMode>,
 )
